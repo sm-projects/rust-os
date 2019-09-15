fd389f7404868635b0bf02fa2d284bed9b1ec42c
smazumder05 <smazumder@gmail.com>
2019-09-15 19:13:29 -0400

    added

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,24 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-   loop {}
-}
-
 // This function is called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
+}// This function is called on panic
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // This is the entry point since the linker looks for a function named _start()
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+   loop {}
 }
